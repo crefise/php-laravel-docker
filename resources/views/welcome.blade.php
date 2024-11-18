8e0e7638e5cd2cbfba645ac69e5100bd406d117f
Volodymyr Ponomarenko <volodymyr.ponomarenko@danads.se>
2024-11-18 18:00:34 +0200

    Init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 <div>
-    <h1></h1>
+    <h1>Hello from lab 4</h1>
     <form method="POST" action="/works">
         @csrf <!-- {{ csrf_field() }} -->
         <h1>Haircuts list:</h1>
